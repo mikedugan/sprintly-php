055f7b92b32c8dac884dc6e6569f4758301137f4
Mike Dugan <mike@mjdugan.com>
2014-11-02 10:11:43 -0500

    add service class

---
+++ src/SprintlyService.php
@@ -0,0 +1,19 @@
+<?php  namespace Dugan\Sprintly; 
+
+class SprintlyService 
+{
+    public function getPeopleRepository()
+    {
+
+    }
+
+    public function getItemsRepository()
+    {
+
+    }
+
+    public function getProductsRepository()
+    {
+
+    }
+}
