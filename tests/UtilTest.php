3a642fcc2b07eba94ab1dc470eb6541203306d12
Mike Dugan <mike@mjdugan.com>
2014-12-10 18:36:14 -0500

    update util test

--- tests/UtilTest.php
+++ tests/UtilTest.php
@@ -27,6 +27,28 @@ class UtilTest extends \PHPUnit_Framework_TestCase
             $this->assertEquals($expected, Util::toSnake($raw));
         }
     }
+
+    /**
+    * @test
+    */
+    public function convertsSnakeToCamel()
+    {
+        $tests = [
+            'SimpleTest' => 'simple_test',
+            'Easy' => 'easy',
+            'Html' => 'html',
+            'SimpleXml' => 'simple_xml',
+            'PdfLoad' => 'pdf_load',
+            'StartMiddleLast' => 'start_middle_last',
+            'AString' => 'a_string',
+            'Some4Numbers234' => 'some4_numbers234',
+            'Test123String' => 'test123_string'
+        ];
+
+        foreach($tests as $raw => $expected) {
+            $this->assertEquals($raw, Util::toCamel($expected));
+        }
+    }
 }
 
 
